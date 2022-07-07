3d7dc22e440f1f150c6a93cafea3e700eb31d047
Ricardo Paiva <werneck.paiva@gmail.com>
2022-07-07 18:09:16 +0200

    Parallel download

--- ezshare_downloader.py
+++ ezshare_downloader.py
@@ -2,10 +2,12 @@ import requests
 import re
 import os.path
 import sys
-
+import threading
+import queue
+from typing import Final, List, Tuple
 
 LINKS_PATTERN = re.compile("<a[^>]*href=\"([^\"]+)\"[^>]*>([^<]+)</a>")
-def extract_anchors(content):
+def extract_anchors(content:str) ->List[Tuple[str, str]]:
     result = LINKS_PATTERN.findall(content)
     result_filtered = [(text.strip(), url) for url, text in result
                         if url.startswith("dir") or "download?" in url]
@@ -22,13 +24,14 @@ def filter_for_medias(anchors):
             if "download?" in link]
 
 
-def download_media(url, destination_folder, media_name):
+def should_download_media(url:str, media_name:str, destination_folder:str)->bool:
     media_path = os.path.join(destination_folder, media_name)
-    if os.path.exists(media_path):
-        print("Skipped " + media_name)
-        return
+    return not os.path.exists(media_path)
+
+def download_media(url:str, media_name:str, destination_folder:str) -> None:
     r = requests.get(url, stream=True)
     total_length = r.headers.get('content-length')
+    media_path = os.path.join(destination_folder, media_name)
     with open(media_path, "wb") as f:
         if total_length is None: # no content length header
             f.write(r.content)
@@ -42,8 +45,13 @@ def download_media(url, destination_folder, media_name):
                 print("\rDownloading %s [%.2f%%]" % (media_name, downloaded_perc), end="")
     print("\rDownloaded %s %s" % (media_name, " " * 10))
 
+def media_downloader_thread(medias_to_download:queue.Queue, destination_folder:str) -> None:
+    while not medias_to_download.empty():
+        media_url, media_name  = medias_to_download.get()
+        download_media(media_url, media_name, destination_folder)
+        medias_to_download.task_done()
 
-def download_images_recursively(url, destination_folder):
+def download_images_recursively(url:str, destination_folder:str) -> None:
     try:
         r = requests.get(url)
     except requests.HTTPError as e:
@@ -54,16 +62,28 @@ def download_images_recursively(url, destination_folder):
     anchors = extract_anchors(r.text)
     sub_pages = filter_for_links(anchors)
     medias = filter_for_medias(anchors)
+    medias_to_download = queue.Queue(len(medias))
     for media_name, media_url in medias:
-        download_media(media_url, destination_folder, media_name)
+        if should_download_media(media_url, media_name, destination_folder):
+            medias_to_download.put((media_url, media_name))
+
+    if len(medias) > 0:
+        print("Total medias:", len(medias))
+        print("Medias to download:", medias_to_download.qsize())
+
+        for _ in range(NUM_THREADS):
+            threading.Thread(target=media_downloader_thread, args=(medias_to_download, destination_folder,)).start()
+
+        medias_to_download.join()
+
     for sub_url in sub_pages:
         download_images_recursively(BASE_URL + sub_url, destination_folder)
 
+NUM_THREADS:Final[int] = 3
+BASE_URL:Final[str] = "http://192.168.4.1/" # "http://ezshare.card/"
+ROOT_URL:Final[str] = BASE_URL + "dir?dir=A:%5CDCIM"
 
-BASE_URL = "http://ezshare.card/"
-ROOT_URL = BASE_URL + "dir?dir=A:%5CDCIM"
-
-destination_folder = '.'
+destination_folder:str = '.'
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
